d1737a33f96729355801eea3e3285cc0ecfe033b
ANIKET PATRA <110764399+aniketpatra96@users.noreply.github.com>
2025-12-23 22:01:50 +0530

    completed app

--- src/@types/pinia.d.ts
+++ src/@types/pinia.d.ts
@@ -1,8 +1,8 @@
 import 'pinia'
-import type { PersistedStateOptions } from 'pinia-plugin-persistedstate'
+import type { PersistenceOptions } from 'pinia-plugin-persistedstate'
 
 declare module 'pinia' {
   interface DefineSetupStoreOptions<Id extends string, S, G, A> {
-    persist?: boolean | PersistedStateOptions
+    persist?: boolean | PersistenceOptions
   }
 }
